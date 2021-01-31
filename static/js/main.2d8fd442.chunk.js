c718f058cbb77960927d20a3614d2956218be868
Gary <garyarzumanyan@gmail.com>
2021-01-31 01:42:07 -0800

    Updates

---
+++ static/js/main.2d8fd442.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpbattleship-game"]=this["webpackJsonpbattleship-game"]||[]).push([[0],{10:function(t,e,n){},11:function(t,e,n){"use strict";n.r(e);var c=n(0),s=n(1),a=n.n(s),i=n(3),r=n.n(i);n(9),n(10);var o=function(){return Object(c.jsx)("div",{className:"App"})};r.a.render(Object(c.jsx)(a.a.StrictMode,{children:Object(c.jsx)(o,{})}),document.getElementById("root"))},9:function(t,e,n){}},[[11,1,2]]]);
+//# sourceMappingURL=main.2d8fd442.chunk.js.map
